4113cf0bbb5793aebde551565635c03f0e5aa55e
300926123 <300926123@PR-A315-31>
2017-08-17 13:57:48 -0400

    Added Welcome Message in my Splash From

--- COMP123-S2017-FinalExam-300926123/SplashForm.Designer.cs
+++ COMP123-S2017-FinalExam-300926123/SplashForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.SplashTimer = new System.Windows.Forms.Timer(this.components);
+            this.WelcomeTom = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SplashTimer
@@ -38,13 +39,29 @@
             this.SplashTimer.Interval = 3000;
             this.SplashTimer.Tick += new System.EventHandler(this.SplashTimer_Tick);
             // 
+            // WelcomeTom
+            // 
+            this.WelcomeTom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WelcomeTom.AutoSize = true;
+            this.WelcomeTom.BackColor = System.Drawing.Color.White;
+            this.WelcomeTom.Font = new System.Drawing.Font("Andy", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeTom.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.WelcomeTom.Location = new System.Drawing.Point(151, 122);
+            this.WelcomeTom.Name = "WelcomeTom";
+            this.WelcomeTom.Size = new System.Drawing.Size(291, 144);
+            this.WelcomeTom.TabIndex = 0;
+            this.WelcomeTom.Text = "Welcome \r\nProfessor!!";
+            this.WelcomeTom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // SplashForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(600, 500);
             this.ControlBox = false;
+            this.Controls.Add(this.WelcomeTom);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -52,11 +69,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer SplashTimer;
+        private System.Windows.Forms.Label WelcomeTom;
     }
 }
