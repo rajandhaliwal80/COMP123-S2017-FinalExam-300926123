3a54b5c7fcbbc19f0ccc08607edb12fed0768505
300926123 <300926123@PR-A315-31>
2017-08-17 13:10:25 -0400

    scaffolded the class ScoreBoard

--- COMP123-S2017-FinalExam-300926123/ScoreBoard.cs
+++ COMP123-S2017-FinalExam-300926123/ScoreBoard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 /// <summary>
 /// Rajandeep Kaur Dhaliwal
@@ -15,9 +16,83 @@ namespace COMP123_S2017_FinalExam_300926123
     class ScoreBoard
     {
         //Fields
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
 
         //Properties
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public TextBox TimeTextBox
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
 
         //Methods
+
+        public ScoreBoard(TextBox scoreTextBox, TextBox timeTextBox, TextBox finalScoreTextBox)
+        {
+
+        }
+
+        public void UpdateTime()
+        {
+
+        }
     }
 }
